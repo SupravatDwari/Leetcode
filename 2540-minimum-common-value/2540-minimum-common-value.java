021e6556d9ca5bbe1a1b28751b94ebe9599a2330
Supravat Dwari <86393808+SupravatDwari@users.noreply.github.com>
2024-03-10 00:37:51 +0530

    Time: 2 ms (81.56%), Space: 62.3 MB (80.15%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,26 +1,15 @@
-public class Solution {
-    public boolean search(int[] nums, int target) {
-        int n = nums.length;
-        int l = 0;
-        int h = n - 1;
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
-            if (nums[mid] == target) {
-                return true;
-            } else if (nums[mid] > target) {
-                h = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return false;
-    }
-
+class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        for (int i = 0; i < n; i++) {
-            if (search(nums2, nums1[i])) {
+       int i = 0, j = 0;
+        int minCommonElement = Integer.MAX_VALUE;
+        
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
                 return nums1[i];
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
             }
         }
         return -1;
